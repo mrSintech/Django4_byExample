290b979bf29dec75bf3b47ac1e44a5a6b3a37f73
mrSintech <imsintech@gmail.com>
2022-10-22 16:02:57 +0330

    init...

---
+++ Preface/main.py
@@ -0,0 +1,19 @@
+"""
+Django is an open-source Python web framework that encourages rapid 
+development and clean, pragmatic design. It takes care of much of the 
+hassle of web development and presents a relatively shallow learning curve 
+for beginner programmers.
+
+PRAGMATIC means:
+writing clean code and managing the code in a manner as 
+efficient as possible, to make it easily understandable, by the person that 
+writes the code, and by the people that will read the code in the future.
+
+Django follows Python’s “batteries included” philosophy, shipping with a rich 
+and versatile set of modules that solve common web-development problems.
+
+BATTERIES INCLUDED means:
+to convey the idea that Python comes with everything you need.
+
+"""
+
